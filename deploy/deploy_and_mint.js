0c5719f46059edfa01ce2c4f185f2680e05a8ed9
Saick Mghase <mghase.sadick1@gmail.com>
2022-09-24 17:06:28 +0300

    fix compile smartcontract

--- deploy/deploy_and_mint.js
+++ deploy/deploy_and_mint.js
@@ -31,30 +31,57 @@ const collectionMetadata = require('../nfts/collection_metadata.json');
         compilerUrl: COMPILER_URL,
         nodes: [{ name: 'testnet', instance: node }],
     });
+    console.log(`Pass: 1`);
     await aeSdk.addAccount(senderAccount, { select: true });
-
-    const CONTRACT = './contracts/NFT.aes';
-    const source = utils.getContractContent(CONTRACT);
-    const fileSystem = utils.getFilesystem(CONTRACT);
-
-    const contract = await aeSdk.getContractInstance({ source, fileSystem });
-
+    console.log(`Pass: 2`);
+    const NFT_CONTRACT = './contracts/NFT.aes';
+    const MARKET_CONTRACT = './contracts/Marketplace.aes';
+    console.log(`Pass: 3`);
+    const nft_source = utils.getContractContent(NFT_CONTRACT);
+    const market_source = utils.getContractContent(MARKET_CONTRACT);
+    console.log(`Pass: 4`);
+    const nftFileSystem = utils.getFilesystem(NFT_CONTRACT);
+    const marketFileSystem = utils.getFilesystem(MARKET_CONTRACT);
+    console.log(`Pass: 5`);
+    
+    //try{
+    const nft_contract = await aeSdk.getContractInstance({ source: nft_source,fileSystem: nftFileSystem });
+    console.log(`Pass: 5.1`);
+    const market_contract = await aeSdk.getContractInstance({ source: market_source,fileSystem: marketFileSystem });
+   
+    // }catch (error) {
+    //     console.log(`errrror====: ${error}`);  
+    // }
+    console.log(`Pass: 6`);
     // deploy
-    await contract.deploy([
+    await nft_contract.deploy([
         collectionMetadata.name,
-        collectionMetadata.symbol
-    ]);
-    console.log(`Contract successfully deployed!`);
-    console.log(`Contract address: ${contract.deployInfo.address}`);
-    console.log(`Tx-Hash: ${contract.deployInfo.txData.hash}`);
-    console.log(`Gas used: ${contract.deployInfo.result.gasUsed}`);
+        collectionMetadata.symbol,
+        ]);
+    console.log(`Pass: 7`);
+    console.log(`NFT Contract successfully deployed!`);
+    console.log(`NFT Contract address: ${nft_contract.deployInfo.address}`);
+    console.log(`Tx-Hash: ${nft_contract.deployInfo.txData.hash}`);
+    console.log(`Gas used: ${nft_contract.deployInfo.result.gasUsed}`);
     console.log(`------------------------------------------------------------------------------------------`);
     console.log(`------------------------------------------------------------------------------------------`);
+
+      // deploy market
+      await market_contract.deploy([
+        nft_contract.deployInfo.address,
+           ]);
+           console.log(`MARKET Contract successfully deployed!`);
+           console.log(`MARKET Contract address: ${market_contract.deployInfo.address}`);
+           console.log(`Tx-Hash: ${market_contract.deployInfo.txData.hash}`);
+           console.log(`Gas used: ${market_contract.deployInfo.result.gasUsed}`);
+           console.log(`--------------------end market----------------------------------------------------------------------`);
+          
+
 
     // mint
     for(let i=0; i<collectionMetadata.nfts.length; i++) {
         const nftMetadataMapStringValues = new Map(Object.entries(collectionMetadata.nfts[i]).map(([k, v]) => [k, typeof v === 'object' ? JSON.stringify(v) : v]));
-        const tx = await contract.methods.mint(
+        const tx = await nft_contract.methods.mint(
             senderAddress,
             {'MetadataMap': [nftMetadataMapStringValues]}
         );
@@ -62,6 +89,17 @@ const collectionMetadata = require('../nfts/collection_metadata.json');
         console.log(`Tx-Hash: ${tx.hash}`);
         console.log(`Gas used: ${tx.result.gasUsed}`);
         console.log(`------------------------------------------------------------------------------------------`);
-        console.log(`------------------------------------------------------------------------------------------`);
+       
+        console.log(`---------------------create  market items ---------------------------------------------------------------------`);
+        console.log(`Pass: 8`);
+        const mtx =await market_contract.call('createMarketItem',[ nft_contract.deployInfo.address,tx.decodedResult,1],{amount : 1}).catch(e =>console.error(e))
+        console.log(`ITEM CREATED  with id '${mtx.decodedResult}'`);
+       
+        //    const mtx = await market_contract.methods.createMarketItem(
+    //          nft_contract.deployInfo.address, //nft address
+    //          tx.decodedResult, //token id
+    //          1
+    //     );
+    console.log(`Pass: 9 ${mtx}`);
     };
 })()
